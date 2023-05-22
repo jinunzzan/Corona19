74f2ef8267bd4fc6dc47745a3ce9514e133b950e
jinunzzan <lovebox6607@gmail.com>
2023-05-22 17:45:30 +0900

    alamofire 로 데이터 받아오기

--- Corona19/ViewController.swift
+++ Corona19/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Charts
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -17,8 +18,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.fetchCovidOverview(completionHandler: {[weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case let .success(result):
+                debugPrint("success \(result)")
+            case let .failure(error):
+                debugPrint("error \(error)")
+                
+            }
+        })
     }
-
+    
+    func fetchCovidOverview(
+        completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+    ){
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey":"GrmoREj7HIbPZMvq28NecyAK6niLBl4sx"
+        ]
+       
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result{
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
+    }
 
 }
 
